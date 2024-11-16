75e81440100f9356d634e304ddac249551aa763e
flagg1391 <flagg1391@gmail.com>
2024-11-16 16:49:35 +0300

    Add better parallax effect

--- Scripts/menu_bg.gd
+++ Scripts/menu_bg.gd
@@ -1,6 +1,14 @@
 extends ParallaxBackground
 
-var menu_speed = 100
+var menu_speed = [0, 0, 0, 15, 30, 0, 45, 60]
+var layers = []
+
+func _ready():
+	for i in range(get_child_count()):
+		layers = get_children() 
+		layers[i].motion_scale = Vector2(menu_speed[i], 0)
 
 func _process(delta: float) -> void:
-	scroll_offset.x -= menu_speed * delta
+	for i in range(layers.size()):
+		var cloud = layers[i]
+		cloud.motion_offset.x -= menu_speed[i] * delta
